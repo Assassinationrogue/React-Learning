4feb91e3b1a22809d56ddfb67b17ad5284f1e2c9
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2022-01-23 16:32:43 +0530

    # class for seasonDisplay

--- weather-app/src/SeasonDisplay.tsx
+++ weather-app/src/SeasonDisplay.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import {FC} from 'react';
+import { Geolocation } from "./interface/geoLocation.interface";
 
 
-const SeasonDisplay: FC = () => {
-  return <div>Season Display</div>;
-};
+class SeasonDisplay extends React.Component<Geolocation>{
+    render(): React.ReactNode {
+        return <div>{this.props.latitude}</div>
+    }
+}
 
 export default SeasonDisplay;
