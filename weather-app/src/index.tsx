5d37cf8c2146d9e7aad1705fdaeaa8c229ce518c
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2022-01-23 10:58:43 +0530

    # geolocation service called

--- weather-app/src/index.tsx
+++ weather-app/src/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
-    return <h1>Hi there</h1>
-}
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.table([
+        {
+          Latitude: position.coords.latitude,
+          Longitude: position.coords.longitude,
+        },
+      ]);
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
+  return <h1>Hi there</h1>;
+};
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-)
+ReactDOM.render(<App />, document.getElementById("root"));
