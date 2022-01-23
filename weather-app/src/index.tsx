f8cf8a6060d8de7990cb907033aa541895b21113
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2022-01-23 10:38:46 +0530

    # init functions

--- weather-app/src/index.tsx
+++ weather-app/src/index.tsx
@@ -1,2 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
+const App = () => {
+    return <h1>Hi there</h1>
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+)
