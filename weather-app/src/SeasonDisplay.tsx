589459fa06560252e6ff6aab0321134a85982e65
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2022-01-23 16:35:45 +0530

    # latitude and longitude set

--- weather-app/src/SeasonDisplay.tsx
+++ weather-app/src/SeasonDisplay.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import {FC} from 'react';
+import { FC } from "react";
 import { Geolocation } from "./interface/geoLocation.interface";
 
-
-class SeasonDisplay extends React.Component<Geolocation>{
-    render(): React.ReactNode {
-        return <div>{this.props.latitude}</div>
-    }
+class SeasonDisplay extends React.Component<Geolocation> {
+  render(): React.ReactNode {
+    return (
+      <div>
+        <div>Latitiude: {this.props.latitude}</div>
+        <div>Longitude: {this.props.longitude}</div>
+      </div>
+    );
+  }
 }
 
 export default SeasonDisplay;
