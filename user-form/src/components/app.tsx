17c018ac238940090a3cee935ebbb6a6575ef0ac
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2022-01-24 18:05:30 +0530

    # app init

---
+++ user-form/src/components/app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBar from "./searchBar";
+
+class App extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
