22cc924f816b2277aae3d3e569018f68891eb137
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2022-01-24 18:22:17 +0530

    # implemented sementic ui

--- user-form/src/components/searchBar.tsx
+++ user-form/src/components/searchBar.tsx
@@ -3,14 +3,16 @@ import React from "react";
 class SearchBar extends React.Component {
   render(): React.ReactNode {
     return (
-      <div>
-        <form>
-          <input type="text" />
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label htmlFor="search">Image Search</label>
+            <input type="text" id="search" />
+          </div>
         </form>
       </div>
     );
   }
 }
-
 
 export default SearchBar;
